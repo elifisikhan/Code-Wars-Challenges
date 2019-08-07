279f486904f879fc44478331f5073fa026e94c8b
Elif Işıkhan <isikhanelif@gmail.com>
2019-08-07 11:31:51 +0300

    Sum of Two Integers

---
+++ Sum_of_Two_Integers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace codewars2
+{
+    /// <summary>
+    /// Given Two integers a, b, find The sum of them, BUT You are not allowed to use the operators + and -
+    /// The numbers(a, b) may be positive , negative values or zeros.
+    /// Returning value will be an integer.
+    /// </summary>
+    public class Sum_of_Two_Integers
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(Add(27,6));
+        }
+        static int Add(int x, int y)
+        {
+            while (y != 0)
+            {
+                int carry = x & y;
+                x ^= y;
+                y = carry << 1;
+            }
+            return x;
+        }
+    }
+}
