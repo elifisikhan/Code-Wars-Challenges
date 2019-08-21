ba12f6371aca7baa0a9e66d56ad06ce296bba284
Elif Işıkhan <isikhanelif@gmail.com>
2019-08-21 14:16:22 +0300

    snail

--- Snail.cs
+++ Snail.cs
@@ -1,5 +1,14 @@
+//Snail Sort
+//Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+//array = [[1,2,3],
+//         [4,5,6],
+//         [7,8,9]]
+//snail(array) #=> [1,2,3,6,9,8,7,4,5]
+
+
 class Program
     {
+
         static int artýs = 0;
 
         static void Main(string[] args)
@@ -64,10 +73,4 @@ class Program
             return yazdýr(artýs, dizi, s);
 	
 	}
-    }
-
- 
-
- 
-
     }
